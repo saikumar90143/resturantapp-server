f4f4b4d4d7fadedd59afe59d1adac5174540714f
saikumar90143 <saikumar.dudala@gmail.com>
2023-09-12 10:43:58 +0530

    latest commit

--- index.js
+++ index.js
@@ -4,35 +4,34 @@ import cors from 'cors'
 import morgan from 'morgan'
 import bodyparser from 'body-parser'
 import dotenv from 'dotenv'
-import MenuRouter from '../Server/ItemRouters/ItemRouter.js'
+import MenuRouter from './ItemRouters/ItemRouter.js'
 dotenv.config()
 
+const app = express()
 
 // port
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-const app=express()
 
-app.use('/uploads',express.static('uploads'))
-// middlewares
+app.use('/uploads', express.static('uploads'))
+    // middlewares
 app.use(morgan('dev'))
-app.use(bodyparser.json({limit:'30mb',extended:true}))
-app.use(bodyparser.urlencoded({limit:"30mb",extended:false}))
+app.use(bodyparser.json({ limit: '30mb', extended: true }))
+app.use(bodyparser.urlencoded({ limit: "30mb", extended: false }))
 app.use(cors())
 
 // routers
-app.use('/api/menu/',MenuRouter)
+app.use('/api/menu/', MenuRouter)
 
 // app.get('/',(req,res)=>{
 //     res.send('hello')
 // })
 
 
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
-    app.listen(PORT,()=>{
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.listen(PORT, () => {
         console.log("server is connected to", PORT)
     })
-}).catch((error)=>{
-   console.log("something wrong",error)
+}).catch((error) => {
+    console.log("something wrong", error)
 })
-
